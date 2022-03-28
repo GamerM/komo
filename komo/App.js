56c9b6aac95d3e18ea5095a43a9bb77885fcadb7
GamerM <computerapple4@gmail.com>
2022-03-27 23:06:23 -0400

    add comments for app.js

--- komo/App.js
+++ komo/App.js
@@ -1,11 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Screens to navigate too
 import homeScreen from "./src/screens/homeScreen"
 import authenticationScreen from './src/screens/authenticationScreen';
 
+//Initlize navigator for application
 const Stack = createNativeStackNavigator();
 
-
+//The main stack on how it will navivgate around the application.
 export default function navigationStack() {
   return (
     <NavigationContainer>
